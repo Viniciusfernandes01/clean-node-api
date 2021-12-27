b2252e6fc3a87ba8d8926cd72d2a07f2ee66597e
viniciusfernandes01 <vinicius.f.fernandes@hotmail.com>
2021-12-27 20:07:21 -0300

    refactor: move sut creation to a factory method

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '!**/**/*protocols.ts*',
     '!**/**/*protocols/index.ts'
   ],
+  watchPathIgnorePatterns: ['globalConfig'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
