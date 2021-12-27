3be2be583d268a5c0a77dfd38d5277b81cf19daa
viniciusfernandes01 <vinicius.f.fernandes@hotmail.com>
2021-12-27 18:34:33 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -5,14 +5,14 @@ import { MongoHelper } from '../helper/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
 
     const { insertedId: id } = result
     const accountById = await accountCollection.findOne({ _id: id })
 
     const account = MongoHelper.map(accountById)
-    console.log(account)
+
     return account
   }
 }
